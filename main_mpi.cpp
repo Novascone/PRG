937953bb8c3355bad177f5cdc30a9079371180aa
Errick Aliifua <ejaliifua1s@gmail.com>
2021-04-11 20:49:12 -0600

    This is not working. However, it's a lot of code to not push. A lot of MPI stuff has been added.

--- main_mpi.cpp
+++ main_mpi.cpp
@@ -9,12 +9,13 @@
 
 using namespace std;
 
+int mpiRank, size, data;
+
 void print(vector< vector<double> > &M);
 void change(vector< vector<double> > &M, int r1, int r2);
 void scale(vector< vector<double> > &M, int r1, double s);
 void addUp(vector< vector<double> > &M, int r1, int r2, double s);
-void creatMatrix();
-double gje(vector< vector<double> > &M1);
+double gje(vector< vector<double> > &M, vector<double> &B, int numberOfProcs);
 
 // pivot is equal to int i
 // master process get matrix and does swaps
@@ -25,8 +26,6 @@ double gje(vector< vector<double> > &M1);
 // keep track of time? Maybe
 
 int main(int argc,char *argv[]) {
-    int mpiRank, size;
-    int data;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MCW, &mpiRank); 
     MPI_Comm_size(MCW, &size); 
@@ -34,6 +33,7 @@ int main(int argc,char *argv[]) {
     int A[3][4] = {{0.0 ,1.0 ,-3.0, -5.0}, {2.0, 3.0, -1.0, 7.0}, {4.0, 5.0, -2.0, 10.0}};
 
     vector< vector<double> > M(3);
+    vector<double> B(3);
 
     for (int i = 0; i < 3; ++i) {
         M[i] = vector<double>(4);
@@ -42,8 +42,7 @@ int main(int argc,char *argv[]) {
         }
     }
 
-
-    gje(M);
+    gje(M, B, size);
 
     MPI_Finalize();
     return 0;
@@ -82,36 +81,83 @@ void addUp(vector< vector<double> > &M, int r1, int r2, double s){
     print(M);
 }
 
-double gje(vector< vector<double> > &M1){
+void gje(vector< vector<double> > &M, vector<double> &B, int numberOfProcs){
+    vector< vector<double> > temp(M.size())
 
-    // Looping through X
-    for(int i = 0; i < M1.size(); i++){
-        // Get a non-zero pivot
-        if(M1[i][i] == 0){
-            for(int j = 0; j < M1.size(); j++){
-                if(M1[j][i] != 0){
-                    change(M1, i, j);
+    for (int i = 0; i <  temp.size(); ++i) {
+        temp[i] = vector<double>(M[0].size());
+    }
+
+    bool done = false;
+    int rowCount = 0;
+    if (mpiRank == 0) {
+        for (int i = 0; i < M.size(); ++i) {
+            if(M[i][i] == 0){
+                for(int j = 0; j < M.size(); j++){
+                    if(M[j][i] != 0){
+                        change(M, i, j);
+                    }
                 }
             }
         }
 
-        // scale pivot to 1
-        if(M1[i][i] == 0){
-            return -1;
-        }else{
-            scale(M1, i, 1.0 / M1[i][i]);
+        // sending row to each process
+        for (int i = rowCount; i < numberOfProcs - 1 && i < M.size(); ++i, ++rowCount) {
+            MPI_Send(&M[0][0], (M[i].size() * M.size()), MPI_DOUBLE, (i + 1), rowCount, MCW);
+            cout << "Master sending row " << i << " to processor " << (i + 1) << endl;
         }
 
-        // Turn other numbers in column to 0
-        for(int j = 0; j < M1.size(); j++){
-            if(M1[j][i] != 0 && j != i){
-                addUp(M1, j, i, -M1[j][i]);
+        while(rowCount < M.size()) {
+            int myFlag, myStatus, received;
+            int epithet = -1;
+            MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, &myFlag, &myStatus);
+            if (myFlag) {
+                MPI_RECV(&received, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, &myStatus);
+                B[myStatus.MPI_TAG] = received;
+                rowCount++;
+
+                if(rowCount == M.size()) {
+                    MPI_SEND(&epithet, 1, MPI_INT, myStatus.MPI_SOURCE, -1, MCW);
+                } else {
+                    MPI_SEND(&M[0][0], (M[i].size() * M.size()), MPI_DOUBLE, myStatus.MPI_SOURCE, rowCount, MCW);
+                }
+            }
+
+        }
+    } else {
+        while(!done) {
+            int myFlag, myStatus;
+            MPI_Iprobe(0, MPI_ANY_TAG, MCW, &myFlag, &myStatus);
+            if (myFlag) {
+                if (myStatus.MPI_TAG == -1) {
+                    done = true;
+                } else {
+                    MPI_RECV(&temp, (M[0].size() * M.size()), MPI_DOUBLE, 0, MPI_ANY_TAG, MCW, &myStatus);
+                    // Looping through X
+                    int pivot = myStatus.MPI_TAG;
+                    for(int i = 0; i < temp.size(); i++){
+
+                        // scale pivot to 1
+                        if(temp[pivot][pivot] == 0){
+                            return -1;
+                        }else{
+                            scale(temp, pivot, 1.0 / temp[pivot][pivot]);
+                        }
+
+                        // Turn other numbers in column to 0
+                        for(int j = 0; j < temp.size(); j++){
+                            if(temp[j][i] != 0 && j != i){
+                                addUp(temp, j, i, -temp[j][i]);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
     cout << "Result:" << endl;
-    print(M1);
+    print(M);
 
     return 0;
 }
