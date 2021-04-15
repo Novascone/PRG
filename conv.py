61d2c07ae463f225339858cce2a369a028620f62
Novascone <54559468+Novascone@users.noreply.github.com>
2021-04-14 20:41:09 -0600

    made conv better

--- conv.py
+++ conv.py
@@ -2,8 +2,8 @@ f = open("mat10.txt", "r")
 f2 = f.read()
 f3 = f2.replace("[", '{')
 f4 = f3.replace("]", '}')
-for i in range(len(f4)):
-    if(next(f4) != "}" and next(f4) != "{"):
-        f5 = f4.replace(".", ".,")
-
-print(f5)
+f5 = f4.replace(".", ".,")
+f6 = f5.replace(",}" ,"}")
+f7 = f6.replace("}}", "}};")
+ff = open("mat.txt", "w")
+ff.write(f7)
